387a14994b4f3cdb1ab6c7a60d2bfd1b25599259
Igor Arecippo <iarecippo@gmail.com>
2022-09-19 18:52:21 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../pages';
+
+describe('Testa o componente <NotFound.js />', () => {
+  it('Testa o heading da página', () => {
+    render(<NotFound />);
+    const heading = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Testa a imagem', () => {
+    render(<NotFound />);
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
